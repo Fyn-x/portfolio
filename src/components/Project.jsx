731263c3190d7697d9e8e3d938278fc158a0d507
Fyn-x <fajri.itclub@gmail.com>
2024-02-19 11:12:47 +0700

    added new component for projects section

---
+++ src/components/Project.jsx
@@ -0,0 +1,82 @@
+import Container from "./Container";
+
+const Project = () => {
+  const projects = [
+    {
+      name: "Company Profile",
+      image: "/img/prev-rsummi.png",
+      preview: "https://rsummi.co.id",
+      lang: ["HTML", "CSS", "JavaScript", "PHP", "NodeJS", "MySQL"],
+      tech: [
+        "CodeIgniter",
+        "ExpressJS",
+        "Flight",
+        "JQuery",
+        "Bootstrap",
+        "SocketIO",
+      ],
+    },
+    {
+      name: "Sistem Jaminan Mutu (Sijamu)",
+      image: "/img/prev-sijamu.png",
+      preview: null,
+      lang: ["HTML", "CSS", "JavaScript", "PHP", "MySQL"],
+      tech: ["JQuery", "Bootstrap"],
+    },
+  ];
+  return (
+    <Container id="projects">
+      <h1 className="text-3xl text-tertiary">Projects</h1>
+      <hr className="bg-quaternary mb-4 w-24 h-1" />
+      <div className="grid grid-cols-3 gap-4">
+        {projects.map((project) => (
+          <div className="card outline-quaternary outline outline-2 shadow-xl">
+            {project.image && (
+              <figure className="max-h-40">
+                <img src={project.image} alt={project.name} />
+              </figure>
+            )}
+            <div className="card-body rounded-lg text-quaternary gap-4">
+              <h2 className="text-2xl font-bold text-tertiary">
+                {project.name}
+              </h2>
+              {project.preview && (
+                <a
+                  className="badge badge-secondary"
+                  target="_blank"
+                  href={project.preview}
+                >
+                  Preview
+                </a>
+              )}
+              <div className="flex flex-col gap-2">
+                <p className="text-md font-bold">Language</p>
+                <hr />
+                <div className="card-actions justify-start">
+                  {project.lang.map((item, index) => (
+                    <div key={index} className="badge badge-outline">
+                      {item}
+                    </div>
+                  ))}
+                </div>
+              </div>
+              <div className="flex flex-col gap-2">
+                <p className="text-md font-bold">Tech</p>
+                <hr />
+                <div className="card-actions justify-start">
+                  {project.tech.map((item, index) => (
+                    <div key={index} className="badge badge-outline">
+                      {item}
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </Container>
+  );
+};
+
+export default Project;
